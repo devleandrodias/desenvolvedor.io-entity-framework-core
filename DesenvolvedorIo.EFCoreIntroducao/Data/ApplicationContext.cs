d3449e6535823d128ea181cebbcf5f4a593886fe
devleandrodias <leandrodbdias@gmail.com>
2020-11-02 09:51:28 -0300

    Adicionando string de conexao banco de dados local

---
+++ DesenvolvedorIo.EFCoreIntroducao/Data/ApplicationContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesenvolvedorIo.EFCoreIntroducao.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=127.0.0.1,1433;Database=dev_io_entity_framework_intro;User Id=sa;Password=m9F857JJXLhss2Mm;");
+        }        
+    }
+}
