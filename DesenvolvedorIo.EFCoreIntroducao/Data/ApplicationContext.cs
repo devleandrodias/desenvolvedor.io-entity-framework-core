b16e7546ff49f3dc82cd0a27cc42d7f917c0347a
devleandrodias <leandrodbdias@gmail.com>
2020-11-02 12:32:32 -0300

    Adicionando registros em massa e instalando logger na aplicacao

--- DesenvolvedorIo.EFCoreIntroducao/Data/ApplicationContext.cs
+++ DesenvolvedorIo.EFCoreIntroducao/Data/ApplicationContext.cs
@@ -1,10 +1,13 @@
 ﻿using DesenvolvedorIo.EFCoreIntroducao.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace DesenvolvedorIo.EFCoreIntroducao.Data
 {
     public class ApplicationContext : DbContext
     {
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(x => x.AddConsole());
+
         public DbSet<Pedido> Pedidos { get; set; }
 
         public DbSet<Produto> Produtos { get; set; }
@@ -13,7 +16,10 @@ namespace DesenvolvedorIo.EFCoreIntroducao.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=127.0.0.1,1433;Database=dev_io_entity_framework_intro;User Id=sa;Password=m9F857JJXLhss2Mm;");
+            optionsBuilder
+                .UseLoggerFactory(_logger)
+                .EnableSensitiveDataLogging()
+                .UseSqlServer("Server=127.0.0.1,1433;Database=dev_io_entity_framework_intro;User Id=sa;Password=m9F857JJXLhss2Mm;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- DesenvolvedorIo.EFCoreIntroducao/Program.cs
+++ DesenvolvedorIo.EFCoreIntroducao/Program.cs
@@ -16,10 +16,62 @@ namespace DesenvolvedorIo.EFCoreIntroducao
 
             //if (existe)
             //{
-                // Executa regra de negocio caso haja migracoes pendentes...
+            // Executa regra de negocio caso haja migracoes pendentes...
             //}
 
-            InserirDados();
+            //InserirDados();
+
+            InserirDadosEmMassa();
+        }
+
+        private static void InserirDadosEmMassa()
+        {
+            Produto produto = new Produto()
+            {
+                Ativo = true,
+                CodigoBarras = "274863983746",
+                Descricao = "MacBook Pro 2018",
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Valor = 1999.99m
+            };
+
+            //Cliente cliente = new Cliente
+            //{
+            //    Nome = "Leandro Dias",
+            //    CEP = "14305092",
+            //    Cidade = "São Paulo",
+            //    Estado = "SP",
+            //    Telefone = "46897654386"
+            //};
+
+            var listaCientes = new[] {
+                    new Cliente
+                {
+                    Nome = "Leandro Dias 2",
+                    CEP = "14305092",
+                    Cidade = "São Paulo",
+                    Estado = "SP",
+                    Telefone = "46897654386"
+                },
+                    new Cliente
+                {
+                    Nome = "Leandro Dias 3",
+                    CEP = "14305092",
+                    Cidade = "São Paulo",
+                    Estado = "SP",
+                    Telefone = "46897654386"
+                }
+            };
+
+            using Data.ApplicationContext db = new Data.ApplicationContext();
+
+            //db.AddRange(produto, cliente);
+
+            db.AddRange(listaCientes);
+
+            int registros = db.SaveChanges();
+
+            Console.WriteLine($"Total de registro(s): {registros}");
         }
 
         private static void InserirDados()
