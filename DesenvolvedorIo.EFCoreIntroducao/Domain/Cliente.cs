965f97889ac8029b1fcc4d104348276c9df4e738
devleandrodias <leandrodbdias@gmail.com>
2020-11-02 10:48:17 -0300

    Utilizando DataAnnotations p/ configurar entidade

--- DesenvolvedorIo.EFCoreIntroducao/Domain/Cliente.cs
+++ DesenvolvedorIo.EFCoreIntroducao/Domain/Cliente.cs
@@ -1,11 +1,18 @@
-﻿namespace DesenvolvedorIo.EFCoreIntroducao.Domain
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DesenvolvedorIo.EFCoreIntroducao.Domain
 {
+    //[Table("Clientes")]
     public class Cliente
     {
+        //[Key]
         public int Id { get; set; }
 
+        //[Required]
         public string Nome { get; set; }
 
+        //[Column("Phone")]
         public string Telefone { get; set; }
 
         public string CEP { get; set; }
